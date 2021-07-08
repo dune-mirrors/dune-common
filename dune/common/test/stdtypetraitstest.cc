04864ae5c03db69af4ccd9e308a17133d5ac9801
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2021-07-08 12:21:36 +0200

    [test][cleanup] Remove tests for Std::is_callable

--- dune/common/test/stdtypetraitstest.cc
+++ dune/common/test/stdtypetraitstest.cc
@@ -14,64 +14,6 @@ int main()
 {
   Dune::TestSuite test;
 
-  // Check is_callable
-  {
-    auto f = [](int /*i*/) { return 0; };
-    using F = decltype(f);
-
-    test.check(Dune::Std::is_callable<F(int)>() == true)
-      << "Dune::Std::is_callable does not accept copy from r-value";
-    test.check(Dune::Std::is_callable<F(int&)>() == true)
-      << "Dune::Std::is_callable does not accept copy from l-value reference";
-    test.check(Dune::Std::is_callable<F(int&&)>() == true)
-      << "Dune::Std::is_callable does not accept copy from r-value reference";
-
-    test.check(Dune::Std::is_callable<F(std::string)>() == false)
-      << "Dune::Std::is_callable accepts invalid argument type";
-    test.check(Dune::Std::is_callable<F(int, int)>() == false)
-      << "Dune::Std::is_callable accepts invalid argument count";
-    test.check(Dune::Std::is_callable<F(int), int>() == true)
-      << "Dune::Std::is_callable does not accept valid return type";
-    test.check(Dune::Std::is_callable<F(int), std::string>() == false)
-      << "Dune::Std::is_callable accepts invalid return type";
-  }
-
-  {
-    auto f = [](const int& /*i*/) {};
-    using F = decltype(f);
-
-    test.check(Dune::Std::is_callable<F(int)>() == true)
-      << "Dune::Std::is_callable does not accept const& temporary from r-value";
-    test.check(Dune::Std::is_callable<F(int&)>() == true)
-      << "Dune::Std::is_callable does not accept const& temporary from l-value reference";
-    test.check(Dune::Std::is_callable<F(int&&)>() == true)
-      << "Dune::Std::is_callable does not accept const& temporary from r-value reference";
-  }
-
-  {
-    auto f = [](int& /*i*/) {};
-    using F = decltype(f);
-
-    test.check(Dune::Std::is_callable<F(int)>() == false)
-      << "Dune::Std::is_callable accepts l-value reference from r-value";
-    test.check(Dune::Std::is_callable<F(int&)>() == true)
-      << "Dune::Std::is_callable does not accept l-value reference from l-value reference";
-    test.check(Dune::Std::is_callable<F(int&&)>() == false)
-      << "Dune::Std::is_callable accepts l-value reference from r-value reference";
-  }
-
-  {
-    auto f = [](int&& /*i*/) {};
-    using F = decltype(f);
-
-    test.check(Dune::Std::is_callable<F(int)>() == true)
-      << "Dune::Std::is_callable does not accept r-value reference from r-value";
-    test.check(Dune::Std::is_callable<F(int&)>() == false)
-      << "Dune::Std::is_callable accepts r-value reference from l-value reference";
-    test.check(Dune::Std::is_callable<F(int&&)>() == true)
-      << "Dune::Std::is_callable does not accept r-value reference from r-value reference";
-  }
-
   // Check negation
   {
     test.check(Dune::Std::negation<std::true_type>::value == false)
