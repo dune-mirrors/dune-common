710592c3f9f089f6eac55a6215fbf19670af191f
Simon Praetorius <simon.praetorius@tu-dresden.de>
2024-06-24 20:52:41 +0200

    Remove the FieldVector<K,1> specialization

--- dune/common/fvector.hh
+++ dune/common/fvector.hh
@@ -10,6 +10,7 @@
 #include <cmath>
 #include <cstdlib>
 #include <cstring>
+#include <stdexcept>
 #include <type_traits>
 #include <utility>
 #include <initializer_list>
@@ -111,11 +112,22 @@ namespace Dune {
     {}
 
     //! Constructor making vector with identical coordinates
+    template <int S = SIZE,
+      std::enable_if_t<(S != 1), int> = 0>
     explicit constexpr FieldVector (const K& k)
         noexcept(std::is_nothrow_copy_assignable_v<K>)
     {
       std::fill(_data.begin(), _data.end(), k);
     }
+
+    //! Constructor with a given scalar
+    template <class T, int S = SIZE,
+      std::enable_if_t<(S == 1), int> = 0,
+      std::enable_if_t<std::is_constructible_v<K, T>, int> = 0>
+    /* implicit */ constexpr FieldVector (const T& t)
+        noexcept(std::is_nothrow_constructible_v<K, T>)
+      : _data{K(t)}
+    {}
 
     //! Construct from a std::initializer_list
     constexpr FieldVector (const std::initializer_list<K>& l)
@@ -153,7 +165,7 @@ namespace Dune {
     explicit FieldVector (const FieldVector<K1, SIZE1>&) = delete;
 
     //! Copy constructor with default behavior
-    FieldVector (const FieldVector&) = default;
+    constexpr FieldVector (const FieldVector&) = default;
 
 
     //! Assignment from another dense vector
@@ -165,6 +177,18 @@ namespace Dune {
       assert(x.size() == dimension);
       for (int i = 0; i < dimension; ++i)
         _data[i] = x[i];
+      return *this;
+    }
+
+    //! Assignment operator from scalar
+    template<class T, int S = SIZE,
+      std::enable_if_t<(S == 1), int> = 0,
+      std::enable_if_t<std::is_assignable_v<K&, const T&>, int> = 0>
+    constexpr FieldVector& operator= (const T& t)
+        noexcept(std::is_nothrow_assignable_v<K&, const T&>)
+    {
+      for (int i = 0; i < dimension; ++i)
+        _data[i] = t;
       return *this;
     }
 
@@ -216,6 +240,16 @@ namespace Dune {
     constexpr const K* data () const noexcept
     {
       return _data.data();
+    }
+
+    //! Conversion operator
+    constexpr operator std::conditional_t<(SIZE==1),const_reference,void> () const noexcept(SIZE == 1)
+      /* requires(SIZE == 1) */
+    {
+      if constexpr(SIZE == 1)
+        return _data[0];
+      else
+        return;
     }
 
     //! Vector space multiplication with scalar
@@ -272,7 +306,7 @@ namespace Dune {
    *  \returns the input stream (in)
    */
   template<class K, int SIZE>
-  std::istream &operator>> (std::istream& in, FieldVector<K, SIZE>& v)
+  std::istream& operator>> (std::istream& in, FieldVector<K, SIZE>& v)
   {
     FieldVector<K, SIZE> w;
     for (int i = 0; i < SIZE; ++i)
@@ -283,146 +317,6 @@ namespace Dune {
   }
 
 #ifndef DOXYGEN
-  template< class K >
-  struct DenseMatVecTraits< FieldVector<K,1> >
-  {
-    typedef FieldVector<K,1> derived_type;
-    typedef K container_type;
-    typedef K value_type;
-    typedef size_t size_type;
-  };
-
-  /** \brief Vectors containing only one component
-   */
-  template<class K>
-  class FieldVector<K, 1> :
-    public DenseVector< FieldVector<K,1> >
-  {
-    K _data;
-    typedef DenseVector< FieldVector<K,1> > Base;
-  public:
-    //! The size of this vector.
-    constexpr static int dimension = 1;
-
-    typedef typename Base::size_type size_type;
-
-    /** \brief The type used for references to the vector entry */
-    typedef K& reference;
-
-    /** \brief The type used for const references to the vector entry */
-    typedef const K& const_reference;
-
-    //===== construction
-
-    /** \brief Default constructor */
-    constexpr FieldVector () noexcept
-      : _data()
-    {}
-
-    /** \brief Constructor with a given scalar */
-    template<class T,
-      std::enable_if_t<std::is_constructible_v<K,T>, int> = 0>
-    constexpr FieldVector (const T& k) noexcept
-      : _data(k)
-    {}
-
-    /** \brief Construct from a std::initializer_list */
-    constexpr FieldVector (const std::initializer_list<K>& l)
-    {
-      assert(l.size() == 1);
-      _data = *l.begin();
-    }
-
-    //! Constructor from static vector of different type
-    template<class T,
-      std::enable_if_t<std::is_constructible_v<K,T>, int> = 0>
-    constexpr FieldVector (const FieldVector<T,1>& x) noexcept
-      : _data(x[0])
-    {}
-
-    //! Constructor from other dense vector
-    template<class T,
-      std::enable_if_t<IsFieldVectorSizeCorrect<T,1>::value, int> = 0,
-      decltype(std::declval<K&>() = std::declval<const T&>()[0], bool{}) = true>
-    FieldVector (const DenseVector<T>& x)
-    {
-      assert(x.size() == 1);
-      _data = x[0];
-    }
-
-    //! copy constructor
-    constexpr FieldVector (const FieldVector&) = default;
-
-    //! copy assignment operator
-    constexpr FieldVector& operator= (const FieldVector&) = default;
-
-    //! assignment from static vector of different type
-    template<class T,
-      decltype(std::declval<K&>() = std::declval<const T&>(), bool{}) = true>
-    constexpr FieldVector& operator= (const FieldVector<T,1>& other) noexcept
-    {
-      _data = other[0];
-      return *this;
-    }
-
-    //! assignment from other dense vector
-    template<class T,
-      std::enable_if_t<IsFieldVectorSizeCorrect<T,1>::value, int> = 0,
-      decltype(std::declval<K&>() = std::declval<const T&>()[0], bool{}) = true>
-    FieldVector& operator= (const DenseVector<T>& other)
-    {
-      assert(other.size() == 1);
-      _data = other[0];
-      return *this;
-    }
-
-    //! Assignment operator for scalar
-    template<class T,
-      decltype(std::declval<K&>() = std::declval<const T&>(), bool{}) = true>
-    constexpr FieldVector& operator= (const T& k) noexcept
-    {
-      _data = k;
-      return *this;
-    }
-
-    //===== forward methods to container
-    static constexpr size_type size () noexcept { return 1; }
-
-    reference operator[] ([[maybe_unused]] size_type i)
-    {
-      DUNE_ASSERT_BOUNDS(i == 0);
-      return _data;
-    }
-    const_reference operator[] ([[maybe_unused]] size_type i) const
-    {
-      DUNE_ASSERT_BOUNDS(i == 0);
-      return _data;
-    }
-
-    //! return pointer to underlying array
-    constexpr K* data () noexcept
-    {
-      return &_data;
-    }
-
-    //! return pointer to underlying array
-    constexpr const K* data () const noexcept
-    {
-      return &_data;
-    }
-
-    //===== conversion operator
-
-    /** \brief Conversion operator */
-    constexpr operator reference () noexcept { return _data; }
-
-    /** \brief Const conversion operator */
-    constexpr operator const_reference () const noexcept { return _data; }
-  };
-
-  /* ----- FV / FV ----- */
-  /* mostly not necessary as these operations are already covered via the cast operator */
-
   //! Binary compare, when using FieldVector<K,1> like K
   template<class K>
   constexpr bool operator> (const FieldVector<K,1>& a, const FieldVector<K,1>& b) noexcept
@@ -465,20 +359,6 @@ namespace Dune {
   constexpr FieldVector<K,1> operator- (const FieldVector<K,1>& a, const K b) noexcept
   {
     return a[0]-b;
-  }
-
-  //! Binary multiplication, when using FieldVector<K,1> like K
-  template<class K>
-  constexpr FieldVector<K,1> operator* (const FieldVector<K,1>& a, const K b) noexcept
-  {
-    return a[0]*b;
-  }
-
-  //! Binary division, when using FieldVector<K,1> like K
-  template<class K>
-  constexpr FieldVector<K,1> operator/ (const FieldVector<K,1>& a, const K b) noexcept
-  {
-    return a[0]/b;
   }
 
   //! Binary compare, when using FieldVector<K,1> like K
@@ -537,13 +417,6 @@ namespace Dune {
   constexpr FieldVector<K,1> operator- (const K a, const FieldVector<K,1>& b) noexcept
   {
     return a-b[0];
-  }
-
-  //! Binary multiplication, when using FieldVector<K,1> like K
-  template<class K>
-  constexpr FieldVector<K,1> operator* (const K a, const FieldVector<K,1>& b) noexcept
-  {
-    return a*b[0];
   }
 
   //! Binary division, when using FieldVector<K,1> like K
