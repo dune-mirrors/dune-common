8660279a7281d67d479163ee3b7386d96dee4a24
Simon Praetorius <simon.praetorius@tu-dresden.de>
2024-09-10 21:02:09 +0200

    Tensor data structure based on mdarray

---
+++ dune/common/tensor.hh
@@ -0,0 +1,393 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+// SPDX-FileCopyrightInfo: Copyright © DUNE Project contributors, see file LICENSE.md in module root
+// SPDX-License-Identifier: LicenseRef-GPL-2.0-only-with-DUNE-exception
+#ifndef DUNE_COMMON_TENSOR_HH
+#define DUNE_COMMON_TENSOR_HH
+
+#include <array>
+#include <type_traits>
+#include <vector>
+
+#include <dune/common/initializerlist.hh>
+#include <dune/common/tensormixin.hh>
+#include <dune/common/tensorspan.hh>
+#include <dune/common/std/extents.hh>
+#include <dune/common/std/layout_right.hh>
+#include <dune/common/std/mdarray.hh>
+#include <dune/common/std/span.hh>
+
+namespace Dune {
+
+//! A special value representing dynamic extents in a tensor
+static constexpr inline std::size_t dynamic = Std::dynamic_extent;
+
+namespace Impl {
+
+template <class Element, std::size_t... extents>
+struct TensorTraits
+{
+  using container_type = std::conditional_t<((extents == Dune::dynamic) || ...),
+    std::vector<Element>, std::array<Element, (extents * ... * std::size_t(1))>>;
+  using extents_type = Std::extents<typename container_type::size_type,extents...>;
+  using storage_type = Std::mdarray<Element, extents_type, Std::layout_right, container_type>;
+};
+
+} // end namespace Impl
+
+
+/**
+ * \brief A tensor of arbitrary rank and individual dimension extents, static
+ * or dynamic, storing field values in a container of corresponding static or
+ * dynamic size.
+ *
+ * \ingroup Tensors
+ * \nosubgrouping
+ *
+ * \tparam Element  The element type stored in the tensor
+ * \tparam extents  Individual static extents or Dune::dynamic
+ **/
+template <class Element, std::size_t... extents>
+class Tensor
+    : public TensorMixin<Tensor<Element,extents...>,
+        typename Impl::TensorTraits<Element,extents...>::storage_type>
+{
+  using self_type = Tensor;
+  using storage_type = typename Impl::TensorTraits<Element,extents...>::storage_type;
+  using base_type = TensorMixin<self_type, storage_type>;
+
+public:
+  using element_type = typename base_type::element_type;
+  using extents_type = typename base_type::extents_type;
+  using value_type = typename base_type::value_type;
+  using index_type = typename base_type::index_type;
+  using layout_type = typename base_type::layout_type;
+  using mapping_type = typename base_type::mapping_type;
+
+  using tensorspan_type = TensorSpan<element_type,extents_type,layout_type>;
+  using const_tensorspan_type = TensorSpan<const element_type,extents_type,layout_type>;
+
+public:
+  /// \brief Inherit constructor from TensorMixin
+  using base_type::base_type;
+
+  /// \name Additional Tensor constructors
+  /// @{
+
+  explicit constexpr Tensor (const value_type& value)
+    : base_type{extents_type{}, value}
+  {}
+
+  /**
+   * \brief Constructor from a brace-init list of values
+   *
+   * \b Example:
+   * \code{.cpp}
+   Tensor<double,2,2> matrix{
+     {1.0,2.0},
+     {2.0, 3.0}
+   };
+   * \endcode
+   **/
+  template <class E = extents_type,
+    std::enable_if_t<(E::rank_dynamic() == 0), int> = 0,
+    std::enable_if_t<std::is_default_constructible_v<E>, int> = 0>
+  constexpr Tensor (NestedInitializerList_t<value_type,extents_type::rank()> init)
+    : Tensor{extents_type{}, init}
+  {}
+
+  /**
+   * \brief Constructor from an extents and brace-init list of values
+   *
+   * \b Example:
+   * \code{.cpp}
+   using Extents = typename Tensor<double,dynamic,dynamic>::extents_type;
+   Tensor<double,dynamic,dynamic> matrix(Extents{2,2},
+   {
+     {1.0,2.0},
+     {2.0, 3.0}
+   });
+   * \endcode
+   **/
+  template <class M = mapping_type,
+    std::enable_if_t<std::is_constructible_v<M,extents_type>, int> = 0>
+  constexpr Tensor (const extents_type& e, NestedInitializerList_t<value_type,extents_type::rank()> init)
+    : Tensor{mapping_type{e}, init}
+  {}
+
+  /**
+   * \brief Constructor from a span of extents and brace-init list of values
+   *
+   * \b Example:
+   * \code{.cpp}
+   Tensor<double,dynamic,dynamic> matrix(std::array{2,2},
+   {
+     {1.0,2.0},
+     {2.0, 3.0}
+   });
+   * \endcode
+   **/
+  template <class Otherindex_type, std::size_t N,
+    std::enable_if_t<std::is_convertible_v<const Otherindex_type&, index_type>, int> = 0,
+    std::enable_if_t<std::is_nothrow_constructible_v<index_type,const Otherindex_type&>, int> = 0,
+    std::enable_if_t<(N == extents_type::rank_dynamic() || N == extents_type::rank()), int> = 0>
+  constexpr Tensor (Std::span<Otherindex_type,N> e,
+                    NestedInitializerList_t<value_type,extents_type::rank()> init)
+    : Tensor{mapping_type{extents_type{e}}, init}
+  {}
+
+  /**
+   * \brief Constructor from a layout-mapping and brace-init list of values
+   *
+   * \b Example:
+   * \code{.cpp}
+   using Extents = typename Tensor<double,dynamic,dynamic>::extents_type;
+   using Mapping = typename Tensor<double,dynamic,dynamic>::mapping_type;
+   Tensor<double,dynamic,dynamic> matrix(Mapping{Extents{2,2}},
+   {
+     {1.0,2.0},
+     {2.0, 3.0}
+   });
+   * \endcode
+   **/
+  constexpr Tensor (const mapping_type& m, NestedInitializerList_t<value_type,extents_type::rank()> init)
+    : base_type{m}
+  {
+    auto it = this->container_data();
+    InitializerList<value_type,extents_type>::apply(init,this->extents(),
+      [&it](value_type value) { *it++ = value; });
+  }
+
+  /// \brief Converting constructor from another TensorMixin
+  template <class D, class B,
+    std::enable_if_t<std::is_constructible_v<base_type, B>, int> = 0>
+  constexpr Tensor (const TensorMixin<D,B>& other)
+    : base_type{Std::mdspan(other)}
+  {}
+
+  /// @}
+
+
+  /// \name Multi index access
+  /// @{
+
+  /**
+   * \brief Subscript operator to access the tensor components using an array of indices.
+   * \b Examples:
+   * \code{c++}
+     Tensor<double,3,3> matrix;
+     matrix[std::array{0,1}] = 7.0;
+     \endcode
+   **/
+  using base_type::operator[];
+
+  /**
+   * \brief Access an element of the tensor using a variadic list of indices.
+   * \b Examples:
+   * \code{c++}
+     Tensor<double,3,3,3> tensor;
+     tensor(0,1,2) = 42.0;
+     \endcode
+   **/
+  using base_type::operator();
+
+  /// @}
+
+
+  /// \name Modifiers
+  /// @{
+
+  /// \brief Change the extents of the tensor and resize the underlying container with given default value
+  template <class V,
+    std::enable_if_t<std::is_convertible_v<V,value_type>, int> = 0>
+  void resize (const extents_type& e, const V& value)
+  {
+    auto container = std::move(*this).extract_container();
+    auto m = mapping_type{e};
+    container.resize(m.required_span_size(), value);
+    static_cast<base_type&>(*this) = base_type{m, std::move(container)};
+  }
+
+  /// \brief Change the extents of the tensor and resize the underlying container
+  void resize (const extents_type& e)
+  {
+    resize(e, value_type(0));
+  }
+
+  /// \brief Change the extents of the tensor by the given individual extents
+  template <class... index_types, class V,
+    std::enable_if_t<(sizeof...(index_types) == extents_type::rank() ||
+                      sizeof...(index_types) == extents_type::rank_dynamic()), int> = 0,
+    std::enable_if_t<(... && std::is_convertible_v<index_types,index_type>), int> = 0,
+    std::enable_if_t<std::is_constructible_v<extents_type,index_types...>, int> = 0,
+    std::enable_if_t<std::is_convertible_v<V,value_type>, int> = 0>
+  void resize (index_types... exts, const V& v)
+  {
+    resize(extents_type{exts...}, v);
+  }
+
+  /// \brief Change the extents of the tensor by the given individual extents
+  template <class... index_types,
+    std::enable_if_t<(sizeof...(index_types) == extents_type::rank() ||
+                      sizeof...(index_types) == extents_type::rank_dynamic()), int> = 0,
+    std::enable_if_t<(... && std::is_convertible_v<index_types,index_type>), int> = 0,
+    std::enable_if_t<std::is_constructible_v<extents_type,index_types...>, int> = 0>
+  void resize (index_types... exts)
+  {
+    resize(extents_type{exts...}, value_type(0));
+  }
+
+  /// @}
+
+
+  /// \name Conversion into mdspan
+  /// @{
+
+  /// \brief Conversion operator to TensorSpan
+  template <class V, class E, class L, class A,
+    std::enable_if_t<std::is_assignable_v<TensorSpan<V,E,L,A>, tensorspan_type>, int> = 0>
+  constexpr operator TensorSpan<V,E,L,A> ()
+  {
+    return tensorspan_type(this->container_data(), this->mapping());
+  }
+
+  /// \brief Conversion operator to TensorSpan
+  template <class V, class E, class L, class A,
+    std::enable_if_t<std::is_assignable_v<TensorSpan<V,E,L,A>, const_tensorspan_type>, int> = 0>
+  constexpr operator TensorSpan<V,E,L,A> () const
+  {
+    return const_tensorspan_type(this->container_data(), this->mapping());
+  }
+
+  /// \brief Conversion function to TensorSpan
+  template <class A = Std::default_accessor<element_type>,
+    std::enable_if_t<
+      std::is_assignable_v<tensorspan_type, TensorSpan<element_type,extents_type,layout_type,A>>, int> = 0>
+  constexpr TensorSpan<element_type,extents_type,layout_type,A>
+  toTensorSpan (const A& accessor = A{})
+  {
+    return TensorSpan<element_type,extents_type,layout_type,A>(this->container_data(), this->mapping(), accessor);
+  }
+
+  /// \brief Conversion function to TensorSpan
+  template <class A = Std::default_accessor<const element_type>,
+    std::enable_if_t<
+      std::is_assignable_v<const_tensorspan_type, TensorSpan<const element_type,extents_type,layout_type,A>>, int> = 0>
+  constexpr TensorSpan<const element_type,extents_type,layout_type,A>
+  toTensorSpan (const A& accessor = A{}) const
+  {
+    return TensorSpan<const element_type,extents_type,layout_type,A>(this->container_data(), this->mapping(), accessor);
+  }
+
+  /// @}
+};
+
+namespace Impl {
+
+template <class M>
+using IsLayoutMapping = std::is_same<M,
+    typename M::layout_type::template mapping<typename M::extents_type>>;
+
+template <class V, class E>
+struct TensorFromExtents;
+
+template <class V, class I, std::size_t... exts>
+struct TensorFromExtents<V, Dune::Std::extents<I, exts...>>
+{
+  using type = Dune::Tensor<V, exts...>;
+};
+
+} // end namespace Impl
+
+
+/// \name Deduction guides
+/// \relates Tensor
+/// @{
+
+template <class index_type, std::size_t... exts, class value_type>
+Tensor (Std::extents<index_type, exts...>, value_type)
+  -> Tensor<value_type, exts...>;
+
+template <class Mapping, class value_type,
+  std::enable_if_t<Impl::IsLayoutMapping<Mapping>::value, int> = 0,
+  std::enable_if_t<(Mapping::extents_type::rank() == 0), int> = 0>
+Tensor (Mapping, value_type)
+  -> Tensor<value_type>;
+
+template <class Mapping, class value_type,
+  std::enable_if_t<Impl::IsLayoutMapping<Mapping>::value, int> = 0,
+  std::enable_if_t<(Mapping::extents_type::rank() == 1), int> = 0>
+Tensor (Mapping, value_type)
+  -> Tensor<value_type, Mapping::extents_type::static_extent(0)>;
+
+template <class Mapping, class value_type,
+  std::enable_if_t<Impl::IsLayoutMapping<Mapping>::value, int> = 0,
+  std::enable_if_t<(Mapping::extents_type::rank() == 2), int> = 0>
+Tensor (Mapping, value_type)
+  -> Tensor<value_type,
+      Mapping::extents_type::static_extent(0),
+      Mapping::extents_type::static_extent(1)>;
+
+template <class Mapping, class value_type,
+  std::enable_if_t<Impl::IsLayoutMapping<Mapping>::value, int> = 0,
+  std::enable_if_t<(Mapping::extents_type::rank() == 3), int> = 0>
+Tensor (Mapping, value_type)
+  -> Tensor<value_type,
+      Mapping::extents_type::static_extent(0),
+      Mapping::extents_type::static_extent(1),
+      Mapping::extents_type::static_extent(2)>;
+
+template <class index_type, std::size_t... exts, class value_type, class Alloc>
+Tensor (Std::extents<index_type, exts...>, value_type, const Alloc&)
+  -> Tensor<value_type, exts...>;
+
+template <class Mapping, class value_type, class Alloc,
+  std::enable_if_t<Impl::IsLayoutMapping<Mapping>::value, int> = 0,
+  std::enable_if_t<(Mapping::extents_type::rank() == 0), int> = 0>
+Tensor (Mapping, value_type, const Alloc&)
+  -> Tensor<value_type>;
+
+template <class Mapping, class value_type, class Alloc,
+  std::enable_if_t<Impl::IsLayoutMapping<Mapping>::value, int> = 0,
+  std::enable_if_t<(Mapping::extents_type::rank() == 1), int> = 0>
+Tensor (Mapping, value_type, const Alloc&)
+  -> Tensor<value_type, Mapping::extents_type::static_extent(0)>;
+
+template <class Mapping, class value_type, class Alloc,
+  std::enable_if_t<Impl::IsLayoutMapping<Mapping>::value, int> = 0,
+  std::enable_if_t<(Mapping::extents_type::rank() == 2), int> = 0>
+Tensor (Mapping, value_type, const Alloc&)
+  -> Tensor<value_type,
+      Mapping::extents_type::static_extent(0),
+      Mapping::extents_type::static_extent(1)>;
+
+template <class Mapping, class value_type, class Alloc,
+  std::enable_if_t<Impl::IsLayoutMapping<Mapping>::value, int> = 0,
+  std::enable_if_t<(Mapping::extents_type::rank() == 3), int> = 0>
+Tensor (Mapping, value_type, const Alloc&)
+  -> Tensor<value_type,
+      Mapping::extents_type::static_extent(0),
+      Mapping::extents_type::static_extent(1),
+      Mapping::extents_type::static_extent(2)>;
+
+template <class V, class I, std::size_t... exts, class L, class C>
+Tensor (Std::mdarray<V,Dune::Std::extents<I,exts...>,L,C>)
+  -> Tensor<V, exts...>;
+
+template <class V, class I, std::size_t... exts, class L, class C>
+Tensor (Std::mdspan<V,Dune::Std::extents<I,exts...>,L,C>)
+  -> Tensor<std::remove_cv_t<V>, exts...>;
+
+/// @}
+
+
+template <class V, std::size_t... exts>
+struct FieldTraits< Tensor<V,exts...> >
+{
+  using field_type = typename FieldTraits<V>::field_type;
+  using real_type = typename FieldTraits<V>::real_type;
+};
+
+} // end namespace Dune
+
+#endif // DUNE_COMMON_TENSOR_HH
---
+++ dune/common/tensorspan.hh
@@ -0,0 +1,150 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+// SPDX-FileCopyrightInfo: Copyright © DUNE Project contributors, see file LICENSE.md in module root
+// SPDX-License-Identifier: LicenseRef-GPL-2.0-only-with-DUNE-exception
+#ifndef DUNE_COMMON_TENSORSPAN_HH
+#define DUNE_COMMON_TENSORSPAN_HH
+
+#include <array>
+#include <type_traits>
+
+#include <dune/common/ftraits.hh>
+#include <dune/common/tensormixin.hh>
+#include <dune/common/std/default_accessor.hh>
+#include <dune/common/std/extents.hh>
+#include <dune/common/std/layout_right.hh>
+#include <dune/common/std/mdspan.hh>
+
+namespace Dune {
+
+/**
+ * \brief A multi-dimensional span with tensor interface.
+ * \nosubgrouping
+ *
+ * \tparam Element  The element type stored in the tensor.
+ * \tparam Extents  The type representing the tensor extents. Must be compatible
+ *                  to `Dune::Std::extents`.
+ * \tparam Layout   A class providing the index mapping from the multi-dimensional
+ *                  index space to a single index usable in the `Container`. See
+ *                  the `Dune::Concept::Layout` concept.
+ * \tparam Accessor A class that defines types and operations to create a reference
+ *                  to a single element stored by a data pointer and index.
+ **/
+template <class Element, class Extents,
+          class Layout = Std::layout_right,
+          class Accessor= Std::default_accessor<Element>>
+class TensorSpan
+    : public TensorMixin<TensorSpan<Element,Extents,Layout,Accessor>,
+        Std::mdspan<Element,Extents,Layout,Accessor>>
+{
+  using self_type = TensorSpan;
+  using storage_type = Std::mdspan<Element,Extents,Layout,Accessor>;
+  using base_type = TensorMixin<self_type,storage_type>;
+
+public:
+  using element_type =	Element;
+  using extents_type = Extents;
+  using layout_type = Layout;
+  using accessor_type = Accessor;
+  using mapping_type = typename base_type::mapping_type;
+
+public:
+  /// \name TensorSpan constructors
+  /// @{
+
+  using base_type::base_type;
+
+  /// \brief Converting constructor
+  template <class V, class E, class L, class A,
+    class M = typename L::template mapping<E>,
+    std::enable_if_t<std::is_constructible_v<mapping_type, const M&>, int> = 0,
+    std::enable_if_t<std::is_constructible_v<accessor_type, const A&>, int> = 0>
+  constexpr TensorSpan (const TensorSpan<V,E,L,A>& other)
+    : base_type{other}
+  {}
+
+  /// \brief Converting move constructor
+  template <class V, class E, class L, class A,
+    class M = typename L::template mapping<E>,
+    std::enable_if_t<std::is_constructible_v<mapping_type, const M&>, int> = 0,
+    std::enable_if_t<std::is_constructible_v<accessor_type, const A&>, int> = 0>
+  constexpr TensorSpan (TensorSpan<V,E,L,A>&& tensor)
+    : base_type{std::move(tensor)}
+  {}
+
+  /// \brief base copy constructor
+  constexpr TensorSpan (const storage_type& tensor)
+    : base_type{tensor}
+  {}
+
+  /// \brief base move constructor
+  constexpr TensorSpan (storage_type&& tensor)
+    : base_type{std::move(tensor)}
+  {}
+
+  /// @}
+};
+
+// deduction guides
+// @{
+
+template <class CArray,
+  std::enable_if_t<std::is_array_v<CArray>, int> = 0,
+  std::enable_if_t<(std::rank_v<CArray> == 1), int> = 0>
+TensorSpan (CArray&)
+  -> TensorSpan<std::remove_all_extents_t<CArray>, Std::extents<std::size_t, std::extent_v<CArray,0>>>;
+
+template <class Pointer,
+  std::enable_if_t<std::is_pointer_v<std::remove_reference_t<Pointer>>, int> = 0>
+TensorSpan (Pointer&&)
+  -> TensorSpan<std::remove_pointer_t<std::remove_reference_t<Pointer>>, Std::extents<std::size_t>>;
+
+template <class element_type, class... II,
+  std::enable_if_t<(... && std::is_convertible_v<II,std::size_t>), int> = 0,
+  std::enable_if_t<(sizeof...(II) > 0), int> = 0>
+TensorSpan (element_type*, II...)
+  -> TensorSpan<element_type, Std::dextents<std::size_t, sizeof...(II)>>;
+
+template <class element_type, class SizeType, std::size_t N>
+TensorSpan (element_type*, Std::span<SizeType,N>&)
+  -> TensorSpan<element_type, Std::dextents<std::size_t, N>>;
+
+template <class element_type, class SizeType, std::size_t N>
+TensorSpan (element_type*, const std::array<SizeType,N>&)
+  -> TensorSpan<element_type, Std::dextents<std::size_t, N>>;
+
+template <class element_type, class IndexType, std::size_t... exts>
+TensorSpan (element_type*, const Std::extents<IndexType,exts...>&)
+  -> TensorSpan<element_type, Std::extents<IndexType,exts...>>;
+
+template <class element_type, class Mapping,
+  class Extents = typename Mapping::extents_type,
+  class Layout = typename Mapping::layout_type>
+TensorSpan (element_type*, const Mapping&)
+  -> TensorSpan<element_type, Extents, Layout>;
+
+template <class Mapping, class Accessor,
+  class DataHandle = typename Accessor::data_handle_type,
+  class Element = typename Accessor::element_type,
+  class Extents = typename Mapping::extents_type,
+  class Layout = typename Mapping::layout_type>
+TensorSpan (const DataHandle&, const Mapping&, const Accessor&)
+  -> TensorSpan<Element, Extents, Layout, Accessor>;
+
+template <class V, class E, class L, class A>
+TensorSpan (Std::mdspan<V,E,L,A>)
+  -> TensorSpan<V,E,L,A>;
+
+/// @}
+
+
+template <class Element, class Extents, class Layout, class Accessor>
+struct FieldTraits< TensorSpan<Element,Extents,Layout,Accessor> >
+{
+  using field_type = typename FieldTraits<Element>::field_type;
+  using real_type = typename FieldTraits<Element>::real_type;
+};
+
+} // end namespace Dune
+
+#endif // DUNE_COMMON_TENSORSPAN_HH
