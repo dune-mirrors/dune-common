cc2c30b94545c40c7d9cf41110e66deb82687252
Simon Praetorius <simon.praetorius@tu-dresden.de>
2024-09-30 13:03:02 +0200

    Use two binary operations instead of a tertiary update functor

--- dune/common/tensordot.hh
+++ dune/common/tensordot.hh
@@ -6,7 +6,9 @@
 #define DUNE_COMMON_TENSORDOT_HH
 
 #include <array>
+#include <functional>
 #include <type_traits>
+#include <utility>
 
 #include <dune/common/integersequence.hh>
 #include <dune/common/std/extents.hh>
@@ -14,46 +16,29 @@
 #include <dune/common/tensor.hh>
 #include <dune/common/typetraits.hh>
 #include <dune/common/concepts/tensor.hh>
+#include <dune/common/std/extents.hh>
+#include <dune/common/std/forceinline.hh>
 
 namespace Dune {
 
 namespace Impl {
 
-/// Functors representing the multiply-add function c += a * b
-/// in various forms, e.g. compound assignment, `std::fma` and with conjugation
-struct MultAdd
+// Extract from the given extents only those dimensions specified by the `dims` sequence
+template <class E, std::size_t... II>
+constexpr auto slicedExtents (const E& extents, std::index_sequence<II...> dims)
 {
-  template <class A, class B>
-  using result_type = std::decay_t<decltype(std::declval<A>() * std::declval<B>())>;
-
-  template <class A, class B, class C>
-  constexpr inline __attribute__((always_inline))
-  void operator() (const A& a, const B& b, C& c) const noexcept
-  {
-    c += a * b;
-  }
-};
-
-// return whether any of the static extents of E1 at positions II... or
-// E2 at positions JJ... is a dynamics extent.
-template <class E1, class E2, std::size_t... II, std::size_t... JJ>
-constexpr bool any_dynamic (std::index_sequence<II...>, std::index_sequence<JJ...>)
-{
-  return (... || (E1::static_extent(II) == Std::dynamic_extent))
-      || (... || (E2::static_extent(JJ) == Std::dynamic_extent));
+  return Std::extents<typename E::index_type, E::static_extent(II)...>{extents.extent(II)...};
 }
 
-// Define the result tensor type of the dot operation.
-// If N indices are contracted, the first rank1-N extents of tensor1 and
-// the last rank2-N extents of tensor2 are combined to build the extents
-// of the output tensor. The storage type is a FixedSizeContainer if all
-// extents are static, otherwise a DynamicSizeContainer.
-template <class V, class E1, std::size_t... II, class E2, std::size_t... JJ>
-constexpr auto makeTensor (const E1& e1, std::index_sequence<II...> iSeq, const E2& e2, std::index_sequence<JJ...> jSeq)
+// Concatenate the given extents to define a new index space
+template <class I1, std::size_t... exts1, class I2, std::size_t... exts2>
+constexpr auto combinedExtents (const Std::extents<I1,exts1...>& e1,
+                                const Std::extents<I2,exts2...>& e2)
 {
-  using I = std::common_type_t<typename E1::index_type, typename E2::index_type>;
-  using E = Std::extents<I, E1::static_extent(II)...,E2::static_extent(JJ)...>;
-  return Tensor<V,inverseMappedExtent<E1::static_extent(II)>...,inverseMappedExtent<E2::static_extent(JJ)>...>(E{I(e1.extent(II))...,I(e2.extent(JJ))...}, V(0));
+  using I = std::common_type_t<I1,I2>;
+  return [&]<std::size_t... II, std::size_t... JJ>(std::index_sequence<II...>, std::index_sequence<JJ...>) {
+    return Std::extents<I, exts1..., exts2...>{I(e1.extent(II))..., I(e2.extent(JJ))...};
+  }(std::make_index_sequence<sizeof...(exts1)>{},std::make_index_sequence<sizeof...(exts2)>{});
 }
 
 // Check that the dynamic extents of e1 and e2 corresponding to the positions II... and JJ... are identical
@@ -94,19 +79,19 @@ for (int l = 0; l < a.extent(aSeq[0]); ++l)
     for (int i = 0; i < a.extent(aSeqInv[0]); ++i)
       for (int j = 0; j < b.extent(bSeqInv[0]); ++j)
         for (int k = 0; k < b.extent(bSeqInv[1]); ++k)
-          update(a(i,l,m), b(l,m,j,k), c(i,j,k))
+          c(i,j,k) = op1(c(i,j,k), op2(a(i,l,m), b(l,m,j,k)))
  * \endcode
  *
- * where `update` typically means `c += a * b` but could be any ternary operation.
+ * where `op1` typically means `plus` and `op2` typically means `multiplies`.
  **/
 template <std::size_t K = 0,
           class A, class ASeq, class ASeqInv,
           class B, class BSeq, class BSeqInv,
-          class C, class Updater>
-constexpr inline __attribute__((always_inline))
+          class C, class BinaryOp1, class BinaryOp2>
+constexpr DUNE_FORCE_INLINE
 void tensorDotImpl (const A& a, ASeq aSeq, ASeqInv aSeqInv,
                     const B& b, BSeq bSeq, BSeqInv bSeqInv,
-                    C& c, const Updater& update,
+                    C& c, BinaryOp1 op1, BinaryOp2 op2,
                     std::array<typename A::index_type,A::rank()> aIndices = {},
                     std::array<typename B::index_type,B::rank()> bIndices = {},
                     std::array<typename C::index_type,C::rank()> cIndices = {})
@@ -117,7 +102,7 @@ void tensorDotImpl (const A& a, ASeq aSeq, ASeqInv aSeqInv,
     for (typename A::index_type k = 0; k < a.extent(I); ++k) {
       aIndices[I] = k;
       bIndices[J] = k;
-      tensorDotImpl<K>(a,tail(aSeq),aSeqInv,b,tail(bSeq),bSeqInv,c,update,aIndices,bIndices,cIndices);
+      tensorDotImpl<K>(a,tail(aSeq),aSeqInv,b,tail(bSeq),bSeqInv,c,op1,op2,aIndices,bIndices,cIndices);
     }
   }
   else if constexpr(aSeqInv.size() > 0) {
@@ -125,7 +110,7 @@ void tensorDotImpl (const A& a, ASeq aSeq, ASeqInv aSeqInv,
     for (typename A::index_type i = 0; i < a.extent(I); ++i) {
       aIndices[I] = i;
       cIndices[K] = i;
-      tensorDotImpl<K+1>(a,aSeq,tail(aSeqInv),b,bSeq,bSeqInv,c,update,aIndices,bIndices,cIndices);
+      tensorDotImpl<K+1>(a,aSeq,tail(aSeqInv),b,bSeq,bSeqInv,c,op1,op2,aIndices,bIndices,cIndices);
     }
   }
   else if constexpr(bSeqInv.size() > 0) {
@@ -133,11 +118,11 @@ void tensorDotImpl (const A& a, ASeq aSeq, ASeqInv aSeqInv,
     for (typename B::index_type j = 0; j < b.extent(J); ++j) {
       bIndices[J] = j;
       cIndices[K] = j;
-      tensorDotImpl<K+1>(a,aSeq,aSeqInv,b,bSeq,tail(bSeqInv),c,update,aIndices,bIndices,cIndices);
+      tensorDotImpl<K+1>(a,aSeq,aSeqInv,b,bSeq,tail(bSeqInv),c,op1,op2,aIndices,bIndices,cIndices);
     }
   }
   else {
-    update(a[aIndices], b[bIndices], c[cIndices]);
+    c[cIndices] = std::invoke(op1, std::move(c[cIndices]), std::invoke(op2, a[aIndices], b[bIndices]));
   }
 }
 
@@ -154,19 +139,33 @@ void tensorDotImpl (const A& a, ASeq aSeq, ASeqInv aSeqInv,
  * The product might be accumulated to the output tensor using the `Updater`
  * function, which by default implements an axpy operation.
  */
-template <class U = Impl::MultAdd,
-          Concept::Tensor A, std::size_t... II,
+template <Concept::Tensor A, std::size_t... II,
           Concept::Tensor B, std::size_t... JJ,
-          Concept::Tensor C>
-constexpr __attribute__((always_inline))
-auto tensordotOut (const A& a, std::index_sequence<II...> aSeq,
-                   const B& b, std::index_sequence<JJ...> bSeq,
-                   C& c, U updater = {})
+          Concept::Tensor C,
+          class BinaryOp1 = std::plus<>, class BinaryOp2 = std::multiplies<>>
+constexpr auto tensordotOut (const A& a, std::index_sequence<II...> aSeq,
+                             const B& b, std::index_sequence<JJ...> bSeq,
+                             C& c, BinaryOp1 op1 = {}, BinaryOp2 op2 = {})
 {
   static_assert(aSeq.size() == bSeq.size());
-  Impl::tensorDotImpl(a,aSeq,difference<A::rank()>(aSeq),
-                      b,bSeq,difference<B::rank()>(bSeq),
-                      c,updater);
+
+  // create integer sequences that do not include the contraction indices
+  const auto aSeqInv = difference<A::rank()>(aSeq);
+  const auto bSeqInv = difference<B::rank()>(bSeq);
+
+  // the extents of a and the extents of b must be compatible to c
+  using EA = typename A::extents_type;
+  using EB = typename B::extents_type;
+  using EC = typename C::extents_type;
+  using SeqK = std::make_index_sequence<C::rank()>;
+  static_assert(Impl::checkStaticExtents<EA,EB>(aSeq, bSeq));
+  static_assert(Impl::checkStaticExtents<EA,EC>(aSeqInv, SeqK{}));
+  static_assert(Impl::checkStaticExtents<EB,EC>(bSeqInv, SeqK{}));
+  assert((Impl::checkExtents(a.extents(), aSeq, b.extents(), bSeq)));
+  assert((Impl::checkExtents(a.extents(), aSeqInv, c.extents(), SeqK{})));
+  assert((Impl::checkExtents(b.extents(), bSeqInv, c.extents(), SeqK{})));
+
+  Impl::tensorDotImpl(a,aSeq,aSeqInv,b,bSeq,bSeqInv,c,std::ref(op1),std::ref(op2));
 }
 
 
@@ -181,18 +180,30 @@ auto tensordotOut (const A& a, std::index_sequence<II...> aSeq,
  * The product might be accumulated to the output tensor using the `Updater`
  * function, which by default implements an axpy operation.
  */
-template <std::size_t N,
-          class U = Impl::MultAdd,
-          Concept::Tensor A, Concept::Tensor B, Concept::Tensor C>
-constexpr __attribute__((always_inline))
-void tensordotOut (const A& a, const B& b, C& c,
-                   std::integral_constant<std::size_t,N> axes = {}, U updater = {})
+template <std::size_t N, Concept::Tensor A, Concept::Tensor B, Concept::Tensor C,
+          class BinaryOp1 = std::plus<>, class BinaryOp2 = std::multiplies<>>
+constexpr void tensordotOut (const A& a, const B& b, C& c,
+                             std::integral_constant<std::size_t,N> axes = {},
+                             BinaryOp1 op1 = {}, BinaryOp2 op2 = {})
 {
   using SeqI = typename StaticIntegralRange<std::size_t,A::rank(),A::rank()-N>::integer_sequence;
   using InvSeqI = std::make_index_sequence<A::rank()-N>;
   using SeqJ = std::make_index_sequence<N>;
   using InvSeqJ = typename StaticIntegralRange<std::size_t,B::rank(),N>::integer_sequence;
-  Impl::tensorDotImpl(a,SeqI{},InvSeqI{},b,SeqJ{},InvSeqJ{},c,updater);
+  using SeqK = std::make_index_sequence<C::rank()>;
+
+  // the extents of a and the extents of b must be compatible to c
+  using EA = typename A::extents_type;
+  using EB = typename B::extents_type;
+  using EC = typename C::extents_type;
+  static_assert(Impl::checkStaticExtents<EA,EB>(SeqI{}, SeqJ{}));
+  static_assert(Impl::checkStaticExtents<EA,EC>(InvSeqI{}, SeqK{}));
+  static_assert(Impl::checkStaticExtents<EB,EC>(InvSeqJ{}, SeqK{}));
+  assert((Impl::checkExtents(a.extents(), SeqI{}, b.extents(), SeqJ{})));
+  assert((Impl::checkExtents(a.extents(), InvSeqI{}, c.extents(), SeqK{})));
+  assert((Impl::checkExtents(b.extents(), InvSeqJ{}, c.extents(), SeqK{})));
+
+  Impl::tensorDotImpl(a,SeqI{},InvSeqI{},b,SeqJ{},InvSeqJ{},c,std::ref(op1),std::ref(op2));
 }
 
 
@@ -214,22 +225,33 @@ void tensordotOut (const A& a, const B& b, C& c,
  * - inner product of 2-tensors: `c = a(i,j) * b(i,j)`
  *     is `tensordot(a,index_sequence<0,1>{},b,index_sequence<0,1>{})`
  */
-template <class U = Impl::MultAdd,
-          Concept::Tensor A, std::size_t... II,
-          Concept::Tensor B, std::size_t... JJ>
+template <Concept::Tensor A, std::size_t... II,
+          Concept::Tensor B, std::size_t... JJ,
+          class BinaryOp1 = std::plus<>, class BinaryOp2 = std::multiplies<>>
 constexpr auto tensordot (const A& a, std::index_sequence<II...> aSeq,
                           const B& b, std::index_sequence<JJ...> bSeq,
-                          U updater = {})
+                          BinaryOp1 op1 = {}, BinaryOp2 op2 = {})
 {
-  // the last extents(II) of a and the extents(JJ) of b must match
-  static_assert(Impl::checkStaticExtents<typename A::ExtentsType,typename B::ExtentsType>(aSeq, bSeq));
+  // the extents(II) of a and the extents(JJ) of b must match
+  using EA = typename A::extents_type;
+  using EB = typename B::extents_type;
+  static_assert(Impl::checkStaticExtents<EA,EB>(aSeq, bSeq));
   assert((Impl::checkExtents(a.extents(), aSeq, b.extents(), bSeq)));
 
-  // create result tensor by collecting the extents of a and b that are not folded
-  using V = typename U::template result_type<typename A::ElementType,typename B::ElementType>;
-  auto c = Impl::makeTensor<V>(a.extents(), difference<A::rank()>(aSeq),
-                               b.extents(), difference<B::rank()>(bSeq));
-  Impl::tensorDotImpl(a,aSeq,difference<A::rank()>(aSeq),b,bSeq,difference<B::rank()>(bSeq),c,updater);
+  // create integer sequences that do not include the contraction indices
+  const auto aSeqInv = difference<A::rank()>(aSeq);
+  const auto bSeqInv = difference<B::rank()>(bSeq);
+
+  // create result extents by collecting the extents of a and b that are not contracted
+  auto cExtents = Impl::combinedExtents(
+    Impl::slicedExtents(a.extents(), aSeqInv),
+    Impl::slicedExtents(b.extents(), bSeqInv));
+
+  using VA = decltype(std::declval<A>()[std::array<typename A::index_type,A::rank()>{}]);
+  using VB = decltype(std::declval<B>()[std::array<typename B::index_type,B::rank()>{}]);
+  using V = std::invoke_result_t<BinaryOp2,VA,VB>;
+  auto c = Tensor{cExtents, V(0)};
+  Impl::tensorDotImpl(a,aSeq,aSeqInv,b,bSeq,bSeqInv,c,std::ref(op1),std::ref(op2));
   return c;
 }
 
@@ -245,16 +267,15 @@ constexpr auto tensordot (const A& a, std::index_sequence<II...> aSeq,
  * - matrix-matrix product: `c(i,j) = a(i,k) * b(k,j)` is `tensordot<1>(a,b)`
  * - inner product of 2-tensors: `c = a(i,j) * b(i,j)` is `tensordot<2>(a,b)`
  **/
-template <std::size_t N,
-          class U = Impl::MultAdd,
-          Concept::Tensor A, Concept::Tensor B>
+template <std::size_t N, Concept::Tensor A, Concept::Tensor B,
+          class BinaryOp1 = std::plus<>, class BinaryOp2 = std::multiplies<>>
 constexpr auto tensordot (const A& a, const B& b,
                           std::integral_constant<std::size_t,N> axes = {},
-                          U updater = {})
+                          BinaryOp1 op1 = {}, BinaryOp2 op2 = {})
 {
   using SeqI = typename StaticIntegralRange<std::size_t,A::rank(),A::rank()-N>::integer_sequence;
   using SeqJ = std::make_index_sequence<N>;
-  return tensordot(a,SeqI{},b,SeqJ{},updater);
+  return tensordot(a,SeqI{},b,SeqJ{},op1,op2);
 }
 
 } // end namespace Dune
