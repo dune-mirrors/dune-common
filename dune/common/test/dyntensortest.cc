ae737a7afcac3fb96c5719f4852187eb8096b58d
Simon Praetorius <simon.praetorius@tu-dresden.de>
2024-12-29 15:20:28 +0100

    Fix some rebase errors

--- dune/common/test/dyntensortest.cc
+++ dune/common/test/dyntensortest.cc
@@ -130,10 +130,6 @@ void checkConstructors(Dune::TestSuite& testSuite)
     Dune::DenseTensor t3{tensor1.toTensorSpan()};
   }
 
-
-  Tensor tensor9(tensor1.toTensorSpan());
-
-
   testSuite.subTest(subTestSuite);
 }
 
