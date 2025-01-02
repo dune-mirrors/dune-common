ab991015cb12e3c9dbc165a6296bd459eb54df3e
Simon Praetorius <simon.praetorius@tu-dresden.de>
2025-01-02 23:27:56 +0100

    Fix the exception checks

--- dune/common/test/ftensortest.cc
+++ dune/common/test/ftensortest.cc
@@ -3,6 +3,7 @@
 // SPDX-FileCopyrightInfo: Copyright © DUNE Project contributors, see file LICENSE.md in module root
 // SPDX-License-Identifier: LicenseRef-GPL-2.0-only-with-DUNE-exception
 
+#include "dune/common/exceptions.hh"
 #include <iostream>
 
 #include <dune/common/fvector.hh>
