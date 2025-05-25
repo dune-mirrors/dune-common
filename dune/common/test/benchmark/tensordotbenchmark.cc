65a4af4a23d125f7d01fca33a72c519932102f37
Simon Praetorius <simon.praetorius@tu-dresden.de>
2025-05-25 18:06:38 +0200

    Add spdx headers

--- dune/common/test/benchmark/tensordotbenchmark.cc
+++ dune/common/test/benchmark/tensordotbenchmark.cc
@@ -1,4 +1,5 @@
-
+// SPDX-FileCopyrightInfo: Copyright © DUNE Project contributors, see file LICENSE.md in module root
+// SPDX-License-Identifier: LicenseRef-GPL-2.0-only-with-DUNE-exception
 #include <span>
 #include <benchmark/benchmark.h>
 
